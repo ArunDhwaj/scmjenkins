5e60810367599d8afc1b9addf37c2f1762616bcf
spb.arun.01@gmail.com <arun.dhwaj@smartbitpixel.co.in>
2018-08-24 17:38:48 +0530

    Code cleanup

--- scmjenkins.java
+++ scmjenkins.java
@@ -8,8 +8,10 @@ public class scmjenkins
 {
     public static void main( String[] args)
     {
-        System.out.println("Successfully integration of SCM and ENKINS !!!");    
+        System.out.println("Successfully integration of SCM and JENKINS !!!");    
         
+        System.out.println("Print Table of one: Successfully !!!");    
+        System.out.println(":: ");    
         for(int i = 1; i < 11; i++)
         {
             System.out.println(i);
