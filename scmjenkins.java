6ddd9d710ab7a769cc0fe6e403ad8912b0f6bf67
spb.arun.01@gmail.com <arun.dhwaj@smartbitpixel.co.in>
2018-08-24 17:00:14 +0530

    Driver class: scmjenkins.java is added to the project

---
+++ scmjenkins.java
@@ -0,0 +1,18 @@
+/**
+@author: Arun Dhwaj,
+@date: 24th Aug, 2018
+@purpose: Integrating SCM and Jenkins POC
+*/
+
+public class scmjenkins
+{
+    public static void main( String[] args)
+    {
+        System.out.println("Successfully integration of SCM and ENKINS !!!");    
+        
+        for(int i = 1; i < 11; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
